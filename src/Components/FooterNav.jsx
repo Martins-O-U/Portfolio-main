c7f0721e27a313299ddccd7f5882a7812b741ce9
HP <martinsonyedikachi@gmail.com>
2020-05-26 23:17:20 +0100

    Editted footer

--- src/Components/FooterNav.jsx
+++ src/Components/FooterNav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {
@@ -11,11 +12,17 @@ import styled from "styled-components";
 export default function Footer() {
   return (
     <StyledDiv>
-      <div className="copy-right">
-        <p>&copy; 2020 Martins O-U</p>
-      </div>
-      <div className="social-handle">
-        <SocialsFoot />
+      <div className="all">
+        <div className="home-contact">
+          <Link to="/" id="home"><p>Home</p></Link>
+          <Link to="/contact" id="home" className="contact"><p>Contact</p></Link>
+        </div>
+        <div className="copy-right">
+          <p>&copy; 2020 Martins O-U</p>
+        </div>
+        <div className="social-handle">
+          <SocialsFoot />
+        </div>
       </div>
     </StyledDiv>
   );
@@ -90,17 +97,58 @@ const StyledDivsoc = styled.div`
 
 
 const StyledDiv = styled.div`
-  display: flex;
-  padding: 3px 3px;
-  justify-content: space-evenly;
-  margin-top: 10px;
-  margin-left: -20px;
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  align-items: center;
-  background-color: #E6E6FA;
-   
+width: 100%;
+background-color: #E6E6FA;
+margin-top: 10px;
+position: fixed;
+bottom: 0;
+
+  .all{
+    display: flex;
+    padding: 3px 3px;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 90%;
+    margin: auto;
+
+    @media only screen and (max-width: 620px){
+      .contact{
+        margin-left: 15px;
+      }
+    }
+
+    @media only screen and (max-width: 590px){
+      .home-contact{
+        display: none;
+      }
+    }
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  .contact{
+    margin-left: 30px;
+  }
+
+  #home{
+    color: black;
+    transition: transform 250ms;
+    display: inline-block;
+  }
+
+  #home:hover{
+    transform: translateY(-3px);
+    color: #2098d1;
+  }
+
+  .home-contact{
+    display: flex;
+    justify-content: space-between;
+    text-decoration: none;
+  }
+
   @media only screen and (max-width:500px){
     p{
       font-size: 14px;
